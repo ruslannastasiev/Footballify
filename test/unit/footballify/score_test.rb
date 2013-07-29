9c7658d098515f44d259c7ae0518d8fe139908b3
ruslannastasiev <ruslan0607@gmail.com>
2013-07-29 16:32:03 +0300

    remove /test

--- test/unit/footballify/score_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module Footballify
-  class ScoreTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
--- test/unit/footballify/season_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module Footballify
-  class SeasonTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
